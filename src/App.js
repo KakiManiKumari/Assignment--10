75efef6e34b4db6eca02a3071ae4e18467ca2c09
KakiManiKumari <kaki.manikumari@gmail.com>
2021-05-24 12:55:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   ]
   return (
     <div className="App">
-      <h1>Students Data</h1>
+      <h1>Students Details</h1>
       <Table striped variant="dark">
         <tbody>
         <tr>
